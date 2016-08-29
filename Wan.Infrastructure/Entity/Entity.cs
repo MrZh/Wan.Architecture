4b4ed9c91189a3811c5d8209d3663db839076244
Lucifer <530622110@qq.com>
2016-08-29 14:33:23 +0800

    更改entity

--- Wan.Infrastructure/Entity/Entity.cs
+++ Wan.Infrastructure/Entity/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Wan.Infrastructure.Entity
 {
@@ -8,5 +9,9 @@ namespace Wan.Infrastructure.Entity
         [StringLength(50)]
         public string Id { get; set; }
 
+        public Entity()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
     }
 }
