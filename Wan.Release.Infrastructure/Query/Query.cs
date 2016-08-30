180f18664be3611a228042b4fe63b2101e1e2349
Lucifer <530622110@qq.com>
2016-08-30 16:40:17 +0800

    删除多余引用

--- Wan.Release.Infrastructure/Query/Query.cs
+++ Wan.Release.Infrastructure/Query/Query.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Wan.Release.Infrastructure.Base;
+﻿using Wan.Release.Infrastructure.Base;
 using Wan.Release.Infrastructure.Extends;
 
 namespace Wan.Release.Infrastructure.Query
