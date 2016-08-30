8aa0ab1473bd388835dd9a30fb77d620aac81d3b
Lucifer <530622110@qq.com>
2016-08-30 16:32:15 +0800

    Query完成

---
+++ Wan.Release.Infrastructure/Base/BaseQuery.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wan.Release.Infrastructure.Base
+{
+    public class BaseQuery
+    {
+        public string Sql { get; protected set; }
+
+        public object Obj { get; protected set; }
+
+        public BaseQuery(string sql, object obj)
+        {
+            Sql = sql;
+            Obj = obj;
+        }
+
+        protected BaseQuery()
+        {
+
+        }
+    }
+}
---
+++ Wan.Release.Infrastructure/Query/Query.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wan.Release.Infrastructure.Base;
+using Wan.Release.Infrastructure.Extends;
+
+namespace Wan.Release.Infrastructure.Query
+{
+    public class Query<T> : BaseQuery where T : Entity.Entity
+    {
+        public Query(T obj, QueryEnum queryEnum = QueryEnum.Equal)
+        {
+            Sql = typeof(T).GetQuerySql(obj, queryEnum);
+            Obj = obj;
+        }
+
+        public Query(object obj, QueryEnum queryEnum = QueryEnum.Equal)
+        {
+            Sql = typeof(T).GetPrimaryQuerySql(queryEnum);
+            Obj = obj;
+        }
+
+        public static BaseQuery InitQuery(T obj, QueryEnum queryEnum = QueryEnum.Equal)
+        {
+            return new BaseQuery(typeof(T).GetQuerySql(obj, queryEnum), obj);
+        }
+
+        public static BaseQuery InitPrimaryQuery(object obj, QueryEnum queryEnum = QueryEnum.Equal)
+        {
+            return new BaseQuery(typeof(T).GetPrimaryQuerySql(queryEnum), obj);
+        }
+    }
+}
